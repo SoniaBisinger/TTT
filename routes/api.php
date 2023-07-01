68bc93fc2046a59d5a1298988d40f7699e60b92b
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-07-01 12:20:37 +0200

    version 2 for JSON output

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// my first endpoint API version 1
+
+// Route::get('greetings', function(){
+//   return "Hello World";
+// });
+
+// version 2 en JSON
+
 Route::get('greetings', function(){
-  return 'Hello World';
+  return response()->json([
+    'status' => 200,
+    'greeting' => 'Hello',
+    'name' => 'Sonia'
+  ], 200);
 });
